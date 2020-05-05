b42eb715dbca72a0e52cbf390617f33bc303c95b
Moody Salem <moody.salem@gmail.com>
2020-05-05 14:00:04 -0400

    Test improvements, export best trade options

--- test/miscellaneous.test.ts
+++ test/miscellaneous.test.ts
@@ -151,5 +151,23 @@ describe('miscellaneous', () => {
       expect(sortedInsert(arr, 3, 3, (a, b) => comp(a, b) * -1)).toEqual(1)
       expect(arr).toEqual([4, 3, 2])
     })
+
+    describe('maxSize of 1', () => {
+      it('empty add', () => {
+        const arr: number[] = []
+        expect(sortedInsert(arr, 3, 1, comp)).toEqual(null)
+        expect(arr).toEqual([3])
+      })
+      it('full add greater', () => {
+        const arr: number[] = [2]
+        expect(sortedInsert(arr, 3, 1, comp)).toEqual(3)
+        expect(arr).toEqual([2])
+      })
+      it('full add lesser', () => {
+        const arr: number[] = [4]
+        expect(sortedInsert(arr, 3, 1, comp)).toEqual(4)
+        expect(arr).toEqual([3])
+      })
+    })
   })
 })
