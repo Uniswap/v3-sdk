49f76aac571a476bf7d796a8a4a7e532601a0749
Lint Action <lint-action@samuelmeuli.com>
2021-01-26 16:26:35 +0000

    Fix code style issues with Prettier

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -5,7 +5,7 @@ import { keccak256 } from '@ethersproject/solidity'
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
 // and load the JSON.
-const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${''/*bytecode*/}`])
+const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${'' /*bytecode*/}`])
 
 describe('constants', () => {
   describe('INIT_CODE_HASH', () => {
