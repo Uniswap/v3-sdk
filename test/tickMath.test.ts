916c0431dfcdaa24b7082ef30522007a1f654c12
Moody Salem <moody.salem@gmail.com>
2021-01-27 14:09:22 -0600

    first tick math implementation

---
+++ test/tickMath.test.ts
@@ -0,0 +1,14 @@
+import TickMath from '../src/utils/tickMath'
+
+describe('TickMath', () => {
+  describe('#getRatioAtTick', () => {
+    it('returns the correct value for min tick', () => {
+      expect(TickMath.getRatioAtTick(-887272).toString()).toEqual('18447437462383981825')
+    })
+    it('returns the correct value for max tick', () => {
+      expect(TickMath.getRatioAtTick(887272).toString()).toEqual(
+        '6276865796315986613307619852238232712866172378830071145882'
+      )
+    })
+  })
+})
