3f4a1fc9772490bf141a7a9809a5009547f5a4d6
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-03-09 03:12:21 -0500

    update w/ sorting in the util fn

--- src/entities/pool.test.ts
+++ src/entities/pool.test.ts
@@ -3,17 +3,43 @@ import { computePoolAddress, Pool } from './pool'
 import { Token, WETH9, TokenAmount, Price, ChainId } from '@uniswap/sdk-core'
 
 describe('computePoolAddress', () => {
+  const factoryAddress = '0x1111111111111111111111111111111111111111'
   it('should correctly compute the pool address', () => {
-    const token0 = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
-    const token1 = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'DAI Stablecoin')
+    const tokenA = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
+    const tokenB = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'DAI Stablecoin')
     const result = computePoolAddress({
-      factoryAddress: '0x1111111111111111111111111111111111111111',
+      factoryAddress,
       fee: FeeAmount.LOW,
-      token0,
-      token1
+      tokenA,
+      tokenB
     })
 
-    expect(result).toEqual('0xDE90B996F781cb20d018400cB226B246F16EC731')
+    expect(result).toEqual('0x77FcC14c0c1993Ce52F9c444391aF56F2C0800B0')
+  })
+
+  it('should correctly compute the pool address', () => {
+    const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
+    const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'DAI Stablecoin')
+    let tokenA = USDC
+    let tokenB = DAI
+    const resultA = computePoolAddress({
+      factoryAddress,
+      fee: FeeAmount.LOW,
+      tokenA,
+      tokenB
+    })
+
+    tokenA = DAI
+
+    tokenB = USDC
+    const resultB = computePoolAddress({
+      factoryAddress,
+      fee: FeeAmount.LOW,
+      tokenA,
+      tokenB
+    })
+
+    expect(resultA).toEqual(resultB)
   })
 })
 
