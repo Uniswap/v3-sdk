cfa3150f18184b30b02a77f9abcc97d05b88d20e
Moody Salem <moody.salem@gmail.com>
2021-04-08 16:27:47 -0500

    remove unused export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export * from './entities'
 export * from './utils'
 
 export * from './constants'
-export * from './errors'
 export * from './nonfungiblePositionManager'
 export * from './swapRouter'
