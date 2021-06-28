5903739d31d6c540d506dded1cb54fbc411d655c
Noah Zinsmeister <noahwz@gmail.com>
2021-06-28 17:20:12 -0400

    quick and dirty

--- src/constants.test.ts
+++ src/constants.test.ts
@@ -1,6 +1,7 @@
-import { POOL_INIT_CODE_HASH } from './constants'
+import { POOL_INIT_CODE_HASH, POOL_INIT_CODE_HASH_OPTIMISM } from './constants'
 
 import { bytecode } from '@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json'
+import { bytecode as SwapRouterBytecode } from '@uniswap/v3-periphery-optimism/artifacts-ovm/contracts/SwapRouter.sol/SwapRouter.json'
 import { keccak256 } from '@ethersproject/solidity'
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
@@ -11,6 +12,11 @@ describe('constants', () => {
   describe('INIT_CODE_HASH', () => {
     it('matches computed bytecode hash', () => {
       expect(COMPUTED_INIT_CODE_HASH).toEqual(POOL_INIT_CODE_HASH)
+    })
+
+    it('optimism', () => {
+      // SwapRouter consumes the PoolAddress library, which contains the bytecode hash...
+      expect(SwapRouterBytecode).toMatch(new RegExp(`.*${POOL_INIT_CODE_HASH_OPTIMISM.slice(2)}.*`))
     })
   })
 })
--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+export const POOL_INIT_CODE_HASH_OPTIMISM = '0x1fc830513acbdb1608b8c18fd3cf4a4bee3329c69bb41d56400401c40fe02fd0'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
