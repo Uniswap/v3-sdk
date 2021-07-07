e93760cf98c19d6e23a636706ec263790853872a
Noah Zinsmeister <noahwz@gmail.com>
2021-07-07 17:19:12 -0400

    add new bytecode hash

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,10 @@ export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
-export const POOL_INIT_CODE_HASH_OPTIMISM = '0x1fc830513acbdb1608b8c18fd3cf4a4bee3329c69bb41d56400401c40fe02fd0'
+export const POOL_INIT_CODE_HASH_OPTIMISM = '0x0c231002d0970d2126e7e00ce88c3b0e5ec8e48dac71478d56245c34ea2f9447'
+
+// historical artifact due to small compiler mismatch
+export const POOL_INIT_CODE_HASH_OPTIMISM_KOVAN = '0x1fc830513acbdb1608b8c18fd3cf4a4bee3329c69bb41d56400401c40fe02fd0'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
