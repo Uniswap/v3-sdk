545fb4eb9003cd20c185fdd62949008695283012
Noah Zinsmeister <noahwz@gmail.com>
2021-07-08 11:57:10 -0400

    fix: add bytecode hash for optimism mainnet (#50)
    * add new bytecode hash
    
    * bump version
    
    * expose manual override for pool init code hash

--- src/utils/computePoolAddress.ts
+++ src/utils/computePoolAddress.ts
@@ -2,18 +2,25 @@ import { defaultAbiCoder } from '@ethersproject/abi'
 import { getCreate2Address } from '@ethersproject/address'
 import { keccak256 } from '@ethersproject/solidity'
 import { Token } from '@uniswap/sdk-core'
-import { FeeAmount, POOL_INIT_CODE_HASH, POOL_INIT_CODE_HASH_OPTIMISM } from '../constants'
+import {
+  FeeAmount,
+  POOL_INIT_CODE_HASH,
+  POOL_INIT_CODE_HASH_OPTIMISM,
+  POOL_INIT_CODE_HASH_OPTIMISM_KOVAN
+} from '../constants'
 
 export function computePoolAddress({
   factoryAddress,
   tokenA,
   tokenB,
-  fee
+  fee,
+  initCodeHashManualOverride
 }: {
   factoryAddress: string
   tokenA: Token
   tokenB: Token
   fee: FeeAmount
+  initCodeHashManualOverride?: string
 }): string {
   const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
   return getCreate2Address(
@@ -22,6 +29,11 @@ export function computePoolAddress({
       ['bytes'],
       [defaultAbiCoder.encode(['address', 'address', 'uint24'], [token0.address, token1.address, fee])]
     ),
-    tokenA.chainId === 10 || tokenA.chainId === 69 ? POOL_INIT_CODE_HASH_OPTIMISM : POOL_INIT_CODE_HASH
+    initCodeHashManualOverride ??
+      (tokenA.chainId === 10
+        ? POOL_INIT_CODE_HASH_OPTIMISM
+        : tokenA.chainId === 69
+        ? POOL_INIT_CODE_HASH_OPTIMISM_KOVAN
+        : POOL_INIT_CODE_HASH)
   )
 }
