74ac8634197c87dd2c2f729762788cbd5fe19cae
Connor Martin <cnr.mrtn@gmail.com>
2021-08-30 16:53:25 -0400

    Update quoter.ts

--- src/quoter.ts
+++ src/quoter.ts
@@ -1,33 +1,9 @@
 import { Interface } from '@ethersproject/abi'
-import { BigintIsh, Currency, CurrencyAmount, Percent, TradeType } from '@uniswap/sdk-core'
+import { Currency, CurrencyAmount, TradeType } from '@uniswap/sdk-core'
 import { encodeRouteToPath } from './utils'
 import { MethodParameters, toHex } from './utils/calldata'
 import { abi } from '@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json'
 import { Route } from './entities'
-
-export interface FeeOptions {
-  /**
-   * The percent of the output that will be taken as a fee.
-   */
-  fee: Percent
-
-  /**
-   * The recipient of the fee.
-   */
-  recipient: string
-}
-
-export interface QuoteOptions {
-  /**
-   * The optional price limit for the trade.
-   */
-  sqrtPriceLimitX96?: BigintIsh
-
-  /**
-   * Optional information for taking a fee on output.
-   */
-  fee?: FeeOptions
-}
 
 export abstract class SwapQuoter {
   public static INTERFACE: Interface = new Interface(abi)
