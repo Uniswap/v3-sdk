d31519979f9a3cdd37951cb065e99b1643842e9e
Moody Salem <moody.salem@gmail.com>
2021-10-13 19:27:24 -0500

    optimism regenesis version

--- src/utils/computePoolAddress.ts
+++ src/utils/computePoolAddress.ts
@@ -2,12 +2,7 @@ import { defaultAbiCoder } from '@ethersproject/abi'
 import { getCreate2Address } from '@ethersproject/address'
 import { keccak256 } from '@ethersproject/solidity'
 import { Token } from '@uniswap/sdk-core'
-import {
-  FeeAmount,
-  POOL_INIT_CODE_HASH,
-  POOL_INIT_CODE_HASH_OPTIMISM,
-  POOL_INIT_CODE_HASH_OPTIMISM_KOVAN
-} from '../constants'
+import { FeeAmount, POOL_INIT_CODE_HASH } from '../constants'
 
 /**
  * Computes a pool address
@@ -15,6 +10,7 @@ import {
  * @param tokenA The first token of the pair, irrespective of sort order
  * @param tokenB The second token of the pair, irrespective of sort order
  * @param fee The fee tier of the pool
+ * @param initCodeHashManualOverride Override the init code hash used to compute the pool address if necessary
  * @returns The pool address
  */
 export function computePoolAddress({
@@ -37,11 +33,6 @@ export function computePoolAddress({
       ['bytes'],
       [defaultAbiCoder.encode(['address', 'address', 'uint24'], [token0.address, token1.address, fee])]
     ),
-    initCodeHashManualOverride ??
-      (tokenA.chainId === 10
-        ? POOL_INIT_CODE_HASH_OPTIMISM
-        : tokenA.chainId === 69
-        ? POOL_INIT_CODE_HASH_OPTIMISM_KOVAN
-        : POOL_INIT_CODE_HASH)
+    initCodeHashManualOverride ?? POOL_INIT_CODE_HASH
   )
 }
