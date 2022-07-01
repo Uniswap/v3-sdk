589c8ec64ae266fc68a8a3d3f71e881a54ba35f2
sawa <jesse.sawa@gmail.com>
2022-07-01 14:11:34 -0400

    feature: add support for chains that use the Quoterv2 contract from smart-router-contracts

--- src/quoter.ts
+++ src/quoter.ts
@@ -1,8 +1,8 @@
 import { Interface } from '@ethersproject/abi'
 import { BigintIsh, Currency, CurrencyAmount, TradeType } from '@uniswap/sdk-core'
-import { encodeRouteToPath } from './utils'
-import { MethodParameters, toHex } from './utils/calldata'
+import { encodeRouteToPath, MethodParameters, toHex } from './utils'
 import IQuoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
+import IQuoterV2 from '@uniswap/swap-router-contracts/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json'
 import { Route } from './entities'
 import invariant from 'tiny-invariant'
 
@@ -14,6 +14,11 @@ export interface QuoteOptions {
    * The optional price limit for the trade.
    */
   sqrtPriceLimitX96?: BigintIsh
+
+  /**
+   * The optional interface abi for the quote
+   */
+  useQuoterV2?: boolean
 }
 
 /**
@@ -31,6 +36,7 @@ export abstract class SwapQuoter {
    * @param route The swap route, a list of pools through which a swap can occur
    * @param amount The amount of the quote, either an amount in, or an amount out
    * @param tradeType The trade type, either exact input or exact output
+   * @param options The optional params including price limit and Quoter contract switch
    * @returns The formatted calldata
    */
   public static quoteCallParameters<TInput extends Currency, TOutput extends Currency>(
@@ -45,21 +51,45 @@ export abstract class SwapQuoter {
 
     if (singleHop) {
       if (tradeType === TradeType.EXACT_INPUT) {
-        calldata = SwapQuoter.INTERFACE.encodeFunctionData(`quoteExactInputSingle`, [
-          route.tokenPath[0].address,
-          route.tokenPath[1].address,
-          route.pools[0].fee,
-          quoteAmount,
-          toHex(options?.sqrtPriceLimitX96 ?? 0)
-        ])
+        if (options.useQuoterV2) {
+          const quoteExactInputSingleParams = {
+            tokenIn: route.tokenPath[0].address,
+            tokenOut: route.tokenPath[1].address,
+            amountIn: quoteAmount,
+            fee: route.pools[0].fee,
+            sqrtPriceLimitX96: toHex(options?.sqrtPriceLimitX96 ?? 0)
+          }
+          const swap = new Interface(IQuoterV2.abi)
+          calldata = swap.encodeFunctionData(`quoteExactInputSingle`, [quoteExactInputSingleParams])
+        } else {
+          calldata = SwapQuoter.INTERFACE.encodeFunctionData(`quoteExactInputSingle`, [
+            route.tokenPath[0].address,
+            route.tokenPath[1].address,
+            route.pools[0].fee,
+            quoteAmount,
+            toHex(options?.sqrtPriceLimitX96 ?? 0)
+          ])
+        }
       } else {
-        calldata = SwapQuoter.INTERFACE.encodeFunctionData(`quoteExactOutputSingle`, [
-          route.tokenPath[0].address,
-          route.tokenPath[1].address,
-          route.pools[0].fee,
-          quoteAmount,
-          toHex(options?.sqrtPriceLimitX96 ?? 0)
-        ])
+        if (options.useQuoterV2) {
+          const quoteExactOutputSingleParams = {
+            tokenIn: route.tokenPath[0].address,
+            tokenOut: route.tokenPath[1].address,
+            amount: quoteAmount,
+            fee: route.pools[0].fee,
+            sqrtPriceLimitX96: toHex(options?.sqrtPriceLimitX96 ?? 0)
+          }
+          const swap = new Interface(IQuoterV2.abi)
+          calldata = swap.encodeFunctionData(`quoteExactOutputSingle`, [quoteExactOutputSingleParams])
+        } else {
+          calldata = SwapQuoter.INTERFACE.encodeFunctionData(`quoteExactOutputSingle`, [
+            route.tokenPath[0].address,
+            route.tokenPath[1].address,
+            route.pools[0].fee,
+            quoteAmount,
+            toHex(options?.sqrtPriceLimitX96 ?? 0)
+          ])
+        }
       }
     } else {
       invariant(options?.sqrtPriceLimitX96 === undefined, 'MULTIHOP_PRICE_LIMIT')
